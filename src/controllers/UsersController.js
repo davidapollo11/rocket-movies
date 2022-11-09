56f7dacd4a59cceb3919313ff7418af9d0d3e77e
David Apollo <davidapollo11@hotmail.com>
2022-11-09 18:34:22 -0300

    created user update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -20,10 +20,50 @@ class UsersController {
     return response.status(201).json()
   }
 
-  update(request, response) {
-    const { name, email, password, old_password, avatar } = request.body
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
 
-    response.json()
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user) {
+      throw new AppError('Usuário não encontrado!')
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Este e-mail já está em uso!')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(password && !old_password) {
+      throw new AppError('Informe a senha antiga!')
+    }
+
+    if(password && old_password) {
+      const checkOldPassord = await compare(old_password, user.password)
+
+      if(!checkOldPassord) {
+        throw new AppError('Senha incorreta!')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    )
+
+    return response.status(200).json()
   }
 }
 
